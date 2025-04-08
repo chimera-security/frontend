0c073bc8bc2fd298eab12cc65d53a0e1b1b979ee
derekdeming <derekdeming17@gmail.com>
2025-04-08 10:13:45 -0400

    fix deployment js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { vercel } from 'vite-plugin-vercel'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vercel()
+  ],
+  base: './',
   server: {
     port: 3000
   }
